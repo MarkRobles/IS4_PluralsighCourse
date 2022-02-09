191b2ff344e49dab9d9209b415cd8b9a950654cd
Marcos Robles <marcos92robles@gmail.com>
2022-02-08 22:56:56 -0600

    # Protecting the api when creating a resource (with roles)

--- src/ImageGallery.API/Controllers/ImagesController.cs
+++ src/ImageGallery.API/Controllers/ImagesController.cs
@@ -66,6 +66,7 @@ namespace ImageGallery.API.Controllers
         }
 
         [HttpPost()]
+        [Authorize(Roles="PayingUser")]
         public IActionResult CreateImage([FromBody] ImageForCreation imageForCreation)
         {
             // Automapper maps only the Title in our configuration
@@ -90,9 +91,10 @@ namespace ImageGallery.API.Controllers
             // fill out the filename
             imageEntity.FileName = fileName;
 
-            // ownerId should be set - can't save image in starter solution, will
-            // be fixed during the course
-            //imageEntity.OwnerId = ...;
+            // set the ownerId on the imageEntity
+            var ownerId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+            //  var ownerId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+            imageEntity.OwnerId = ownerId;
 
             // add and save.  
             _galleryRepository.AddImage(imageEntity);
