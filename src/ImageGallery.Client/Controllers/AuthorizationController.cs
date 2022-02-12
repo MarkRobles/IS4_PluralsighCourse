dd20197b85ee6b95bbd7e854d36b21838aacbf16
Marcos Robles <marcos92robles@gmail.com>
2022-02-12 14:18:36 -0600

    #Cambiar ubicacion de logout

--- src/ImageGallery.Client/Controllers/AuthorizationController.cs
+++ src/ImageGallery.Client/Controllers/AuthorizationController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ImageGallery.Client.Controllers
 {
@@ -7,6 +11,13 @@ namespace ImageGallery.Client.Controllers
         public IActionResult AccessDenied()
         {
             return View();
+        }
+
+        public async Task Logout()
+        {
+            //Clean the cookie
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
         }
     }
 }
